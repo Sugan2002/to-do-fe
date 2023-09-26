18e82294db6f89eb06145765da2b0c63275f3394
Sugan2002 <suganya.parthiban2002@gmail.com>
2023-09-26 15:02:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TodoListComponent from './components/TodoListComponent';
+
+function App() {
+  return (
+    <div className="container">
+      <h3>To Do List</h3>
+      <TodoListComponent/>
+    </div>
+  );
+}
+
+export default App;
